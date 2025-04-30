9eed294f2f97b7d6868ad6e2b4c741e9cd33ba32
Stanislav Malovaniuk <stasmalovaniuk@gmail.com>
2025-04-30 23:21:38 +0300

    Added local connection string for db

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6923d3d4ac7cd57195e921427889172fd0688630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b33de96b0a874ac8de83da97dcb8faa0a833e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
